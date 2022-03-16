e84cd6197e7f0ce34501921f1d012b2f54cca046
Michaellitsxd <68073296+Michaellitsxd@users.noreply.github.com>
2022-03-16 16:15:40 -0500

    Update theloader.lua

--- theloader.lua
+++ theloader.lua
@@ -6,23 +6,6 @@ local win = SolarisLib:New({
    FolderToSave = "TmHub"
 })
 
-
-
-local tab = win:Tab("Game Mods")
-   
-local sec = tab:Section("Doors")
-
-local sec2 = tab:Section("Message Board")
-
-local sec4 = tab:Section("Teleports")
-
- 
- local toggle = sec:Toggle("toggle test", false,"Toggle", function(t)
-    Spam_Door3 = t
- end)
-
-
-
 local tab2 = win:Tab("Player Mods")
    
 local sec5 = tab2:Section("Speed and Jump")
@@ -31,8 +14,14 @@ local slider = sec5:Slider("Walkspeed", 16,400,16,2.5,"Slider", function(t)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = t
 end)
 local button = sec5:Button("Aimbot", function(t)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-Script/main/Aimbot%20Script.lua"))()
-end)    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Herrtt/AimHot-v8/master/Main.lua", true))()
+end)   
+local button = sec5:Button("r15 Fly", function(t)
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/MichaelDev337/somehub/main/flyscipr.lua'),true))()
+end)
+local button = sec5:Button("Rspy", function(t)
+    loadstring(game:HttpGet("https://gist.githubusercontent.com/luatsuki/c75a272fb67bccc22bd1b6add92ee267/raw/56375f8536aeca0cc84b44032312efb0fa5b7fa0/Spy"))()
+end)
 local slider = sec5:Slider("JumpPower", 50,400,50,2.5,"Slider", function(t)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = t
 end)
